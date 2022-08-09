9712b879d1c6f7c59bc18a512beeb8b1e6f51335
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-08-09 11:20:19 +0600

    Initial Commit

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FcGlobe } from 'react-icons/fc';
+const NavBar = () => {
+  return (
+    <div className='nav'>
+      <div className='nav__Logo'>
+        <FcGlobe />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
